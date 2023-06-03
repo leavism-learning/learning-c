5a5ead9b095788dc6ac9bdffc31e5f400e8e2752
Huy Dang <ghuydang@gmail.com>
2023-06-02 18:07:09 -0700

    Arrays
    Learned how to make arrays. Be wary as array indexes start at 0 but you create arrays starting the capacity.

--- main.c
+++ main.c
@@ -9,5 +9,16 @@ int main() {
     a = b - c + d * e;
     printf("a = b - c + d * e = %d\n", a);
 
+
+    int numbers[5];
+    int sum = 0;
+    numbers[0] = 1;
+    numbers[1] = 3;
+    numbers[2] = 4;
+    numbers[3] = 10;
+    numbers[4] = 25;
+    sum = numbers[0] + numbers[3];
+    printf("1 + 10 = %d\n", sum);
+
     return 0;
 }
