6e6a792de638031abab7127232f2bfc9483f3254
Huy Dang <ghuydang@gmail.com>
2023-06-06 11:03:17 -0700

    Pointers

---
+++ pointer.c
@@ -0,0 +1,18 @@
+//
+// Created by Huy Dang on 6/5/23.
+//
+// a pointer is a simple integer variable
+// that holds a memory address that points to
+// a value, instead of holding the actual value itself
+
+#include <printf.h>
+
+// The computer's memory is a sequential store of data,
+// and a pointer points to a specific part of the memory
+
+int main() {
+    char * name = "John";
+    int i;
+
+    return 0;
+}
