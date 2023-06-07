ba46844b8e2d04ccfde2aa920c85ee33b3eee201
Huy Dang <ghuydang@gmail.com>
2023-06-06 21:17:05 -0700

    Another typedef example

--- structures.c
+++ structures.c
@@ -6,8 +6,23 @@ typedef struct{
     int y;
 } point;
 
+#include <stdio.h>
+
+/* define the person struct here using the typedef syntax */
+typedef struct {
+    char * name;
+    int age;
+} person;
+
 int main() {
     point p;
     p.x = 10;
     p.y = 4;
+
+    person john;
+
+    /* testing code */
+    john.name = "John";
+    john.age = 27;
+    printf("%s is %d years old.", john.name, john.age);
 }
