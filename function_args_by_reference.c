81576dfc7612504ae075ddacd3c9a47de25fb57f
Huy Dang <ghuydang@gmail.com>
2023-06-08 22:33:23 -0700

    Pass struct reference

--- function_args_by_reference.c
+++ function_args_by_reference.c
@@ -4,13 +4,36 @@
 
 #include <printf.h>
 
+
+typedef struct {
+    int x;
+    int y;
+} point;
+
+void addOne(int *n);
+void move(point *p);
+
 void addOne(int *n) {
     (*n)++;
+}
+
+void move(point * p) {
+    (*p).x++;
+    (*p).y++;
 }
 
 int main() {
     int val = 0;
     addOne(&val);
     printf("%d\n", val);
+
+    point me;
+    me.x = 10;
+    me.y = 0;
+
+    move(&me);
+    printf("%d\n", me.x);
+    printf("%d\n", me.y);
+
     return 0;
 }
