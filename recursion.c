c721cd74ebe0cddbbe7239a123bf5096f50ece77
Huy Dang <ghuydang@gmail.com>
2023-06-20 08:20:52 -0700

    Another recursion example with factorials

--- recursion.c
+++ recursion.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 unsigned int multiply(unsigned int x, unsigned int y);
+unsigned int factorial(unsigned int x);
 
 unsigned int multiply(unsigned int x, unsigned int y) {
     if (x == 1) {
@@ -16,9 +17,14 @@ unsigned int multiply(unsigned int x, unsigned int y) {
     return 0;
 }
 
-
+unsigned int factorial(unsigned int x) {
+    if (x <= 1) { return 1; }
+    else { return x * factorial(x-1); }
+}
 
 int main() {
-    printf("3 time 5 is %d\n", multiply(3,5));
+    printf("3 time 5 is %d\n", multiply(3,5));    int num = 5;
+    printf("%d! = %d", num, factorial(num));
+
     return 0;
 }
