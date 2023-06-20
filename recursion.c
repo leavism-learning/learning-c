a931fb0b8ead6ff0f3c5a7d58085bf0496f8b11f
Huy Dang <ghuydang@gmail.com>
2023-06-20 06:48:15 -0700

    Simple recursion practice

---
+++ recursion.c
@@ -0,0 +1,24 @@
+//
+// Created by Huy Dang on 6/21/23.
+//
+
+#include <stdio.h>
+
+unsigned int multiply(unsigned int x, unsigned int y);
+
+unsigned int multiply(unsigned int x, unsigned int y) {
+    if (x == 1) {
+        return y;
+    } else if (x > 1) {
+        return y + multiply(x - 1, y);
+    }
+
+    return 0;
+}
+
+
+
+int main() {
+    printf("3 time 5 is %d\n", multiply(3,5));
+    return 0;
+}
