a85cdfe9547e414fecdcd0690d78287314143b71
Huy Dang <ghuydang@gmail.com>
2023-06-20 09:47:07 -0700

    Initial commit for a linked list
    I guess the best way to learn and practice mallocing and using pointers is by making a linked list. This commit is the base and initial starting point for making a linked list, just in case I fuck up somewhere.

--- linked_list.c
+++ linked_list.c
@@ -1,3 +1,15 @@
 //
 // Created by Huy Dang on 6/21/23.
 //
+#include <stdio.h>
+
+typedef struct {
+    int val;
+    struct node * next;
+} node;
+
+int main() {
+    node * head = NULL;
+
+    return 0;
+}
