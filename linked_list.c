e4dc2935aad7bc728ae8ad41fbf4ad96c6afb7d2
Huy Dang <ghuydang@gmail.com>
2023-06-21 09:29:32 -0700

    Function to traverse linked list

--- linked_list.c
+++ linked_list.c
@@ -9,14 +9,25 @@ typedef struct node {
     struct node * next;
 } node;
 
+void print_list(node * head);
+
+void print_list(node * head) {
+    node * current = head;
+    while (current != NULL) {
+        printf("%d\n", current->val);
+        current = current->next;
+    }
+}
+
 int main() {
     node * head = NULL;
     head = (node *) malloc(sizeof(node));
     head->val = 1;
     head->next = (node *) malloc(sizeof(node));
-    
+
     head->next->val = 2;
     head->next->next = NULL;
 
     return 0;
 }
+
