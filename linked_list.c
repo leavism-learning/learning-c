0142886bca6f20c9cd127f825cce94f6e1e69ef1
Huy Dang <ghuydang@gmail.com>
2023-06-21 09:27:09 -0700

    Adding a second node in the worst way possible

--- linked_list.c
+++ linked_list.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+typedef struct node {
     int val;
     struct node * next;
 } node;
@@ -12,9 +12,11 @@ typedef struct {
 int main() {
     node * head = NULL;
     head = (node *) malloc(sizeof(node));
-
-    if (head == NULL) { return 1; }
+    head->val = 1;
+    head->next = (node *) malloc(sizeof(node));
     
+    head->next->val = 2;
+    head->next->next = NULL;
 
     return 0;
 }
